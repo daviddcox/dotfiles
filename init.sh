2a29547d4126fbbc80df685834a1323cdd2f41d9
David Cox <cox407@purdue.edu>
2025-02-03 16:01:42 +0000

    fixed file path in init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,16 @@ DOTFILES_DIR="$HOME/dotfiles"
 dotfiles=(.bashrc .vimrc)
 
 for file in "${dotfiles[@]}"; do
-    ln -sf "$DOTFILES_DIR/.$file" "$HOME/.$file"
+    ln -sf "$DOTFILES_DIR/$file" "$HOME/$file"
 done
+
+# Download and install Gruvbox theme for Vim
+GRUVBOX_DIR="$HOME/.vim/pack/default/start/gruvbox"
+
+if [ ! -d "$GRUVBOX_DIR" ]; then
+    git clone https://github.com/morhetz/gruvbox.git "$GRUVBOX_DIR"
+fi
+
+mkdir -p ~/.vim/colors
+cp "$GRUVBOX_DIR/colors/gruvbox.vim" ~/.vim/colors/
 
