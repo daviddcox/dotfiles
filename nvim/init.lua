f1f9fdd61b13a989d23c68f9b3b089688e1c2aad
David Cox <cox407@purdue.edu>
2025-04-22 11:58:06 -0400

    more cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,3 @@
--- Set leader key to space
-vim.g.mapleader = " "
-
 -- General settings
 vim.opt.number = true             -- Show line numbers
 vim.opt.relativenumber = true     -- Relative line numbers
@@ -16,54 +13,55 @@ vim.opt.undofile = true           -- Persistent undo
 vim.opt.termguicolors = true      -- Enable true color support
 vim.opt.path:append("**")
 
--- Enable system clipboard
-vim.opt.clipboard:append("unnamedplus")
+-- Helper Functions
+function GrepSearch()
+	require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+end
 
--- Keybindings
-vim.keymap.set('n', 'n', 'nzzzv', { noremap = true, silent = true })
-vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>x', ':x<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>h', ':noh<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>|', ':vsplit<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>-', ':split<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>u', ':b#<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tr', ':NERDTreeRefreshRoot<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>te', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tt', ':NERDTreeFind<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>cp', ':cprev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>cn', ':cnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>os', ':Obsession<CR>', { noremap = true, silent = true })  -- start
-vim.api.nvim_set_keymap('n', '<leader>ox', ':Obsession!<CR>', { noremap = true, silent = true })  -- stop
-vim.api.nvim_set_keymap('n', '<leader>pf', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope git_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>pb', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>pe',
-function() require('telescope.builtin').diagnostics({ bufnr = 0 }) end,
-  { desc = "Telescope buffer diagnostics" }
-)
-vim.keymap.set('n', '<leader>ps',
-function()
-  require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
-end, { noremap = true, silent = true })
+function ErrorSearch()
+	require('telescope.builtin').diagnostics({ bufnr = 0 })
+end
 
-
-vim.api.nvim_set_keymap('n', '<leader>s', [[:lua SaveSession()<CR>]], { noremap = true, silent = true })
-function SaveSession()
+function GenTags()
   vim.fn.jobstart("ctags -R -L <(git ls-files)", {
     stdout_buffered = true,
     stderr_buffered = true,
     shell = true,
   })
-  vim.cmd("mks!")
 end
 
+-- Set leader key to space
+vim.g.mapleader = " "
+
+-- Keybindings
+vim.keymap.set('n', 'n', 'nzzzv', { noremap = true, silent = true })
+vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>x', ':x<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>h', ':noh<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>|', ':vsplit<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>-', ':split<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>u', ':b#<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tr', ':NERDTreeRefreshRoot<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>te', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tt', ':NERDTreeFind<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cp', ':cprev<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cn', ':cnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>os', ':Obsession<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ox', ':Obsession!<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>pf', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-p>', ':Telescope git_files<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>pb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>pe', ErrorSearch, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ps', GrepSearch, { noremap = true, silent = true })
+
+-- Prettier on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.json", "*.css", "*.html" },
   command = "Prettier",
 })
 
-
+-- Include lazy config
 require("config.lazy")
 
 -- Set theme
